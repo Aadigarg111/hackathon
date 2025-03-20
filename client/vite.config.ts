b2fd37d2734b8931b87f17679f0e3d24d167a916
Aadi <aadileetcode@gmail.com>
2025-03-20 16:47:57 +0530

    fix: update Vercel and Vite configurations for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig, loadEnv } from "vite";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -12,9 +13,16 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    base: '/',
     build: {
       outDir: 'dist',
-      sourcemap: true
+      sourcemap: true,
+      emptyOutDir: true
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     },
     server: {
       proxy: {
